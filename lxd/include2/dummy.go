906f0da200a5e08cb5e29b6bf367bdc157961ce7
Denis Manente <denis.manente@automatic-server.com>
2020-07-22 12:28:52 +0200

    init

---
+++ lxd/include2/dummy.go
@@ -0,0 +1,3 @@
+// This file only exists to mark this folder as go package so `go mod vendor` checks out this folder into `vendor`
+
+package include2
---
+++ lxd/include3/dummy.go
@@ -0,0 +1,4 @@
+// This file only exists to mark this folder as go package so `go mod vendor` checks out this folder into `vendor`
+// +build dummy
+
+package include2
