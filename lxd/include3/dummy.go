a83a167b5fa791de434e47f9a1e7ba9611638ca3
Denis Manente <denis.manente@automatic-server.com>
2020-07-22 12:42:48 +0200

    add a package so we can import

--- lxd/include3/dummy.go
+++ lxd/include3/dummy.go
@@ -1,4 +1,4 @@
 // This file only exists to mark this folder as go package so `go mod vendor` checks out this folder into `vendor`
 // +build dummy
 
-package include2
+package include3
---
+++ lxd/package.go
@@ -0,0 +1,4 @@
+// This file is just a package so we have at least one package we can make a dummy import to
+package lxd
+
+const Something = "world"
