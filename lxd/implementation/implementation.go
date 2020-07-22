9885b8fb3b6c806cff46297270a83b1aecc33309
Denis Manente <denis.manente@automatic-server.com>
2020-07-22 13:17:08 +0200

    describe implementation

--- lxd/implementation/implementation.go
+++ lxd/implementation/implementation.go
@@ -1,5 +1,8 @@
+// This package represents an implementation where others import from
 package implementation
 
 import (
 	_ "github.com/dionysius/issue-lxd-7677-lib/lxd/include4"
 )
+
+const Useful = "maybe"
